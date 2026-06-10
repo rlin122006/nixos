bc0f78da3f66a2ec7d49192137d44f0848b1db3b
Riley <rlin122006@gmail.com>
2026-06-10 12:00:01 +0800

    removed ssh agent for now

--- configuration/network.nix
+++ configuration/network.nix
@@ -32,11 +32,6 @@
   services.blueman.enable = true;
 
   programs.ssh = {
-    startAgent = true;
-    extraConfig = ''
-      AddKeysToAgent yes
-    '';
-
     knownHosts = {
       "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
     };
