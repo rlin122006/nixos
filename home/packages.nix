c71c24573b6a7142397458f142ff56782292558b
Riley <rlin122006@gmail.com>
2026-06-18 17:13:33 -0400

    firefox

--- home/packages.nix
+++ home/packages.nix
@@ -24,20 +24,6 @@
     enable = true;
 
     policies = {
-      DisableTelemetry = true;
-      DisableFirefoxStudies = true;
-      DisableFirefoxAccounts = true;
-      DisablePocket = true;
-
-      ExtensionSettings = {
-        "uBlock@raymondhill.net" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          installation_mode = "force_installed";
-          private_browsing = true;
-          default_area = "menupanel";
-        };
-      };
-
       Preferences = {
         "ui.key.menuAccessKeyFocuses" = false; 
       };
