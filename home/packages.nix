62f01e817a769ca262006cef09a31a932227db01
Riley <rlin122006@gmail.com>
2026-06-18 17:40:43 -0400

    firefox

--- home/packages.nix
+++ home/packages.nix
@@ -35,7 +35,6 @@
         "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
         "browser.newtabpage.activity-stream.feeds.topsites" = false;
         "browser.newtabpage.activity-stream.showWeather" = false;
-        "browser.newtabpage.activity-stream.newtabWallpaper.enabled" = false;
       };
     };
   };
