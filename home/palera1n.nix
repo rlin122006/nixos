fae740e74ad7d474453335a1bc6456ebafe63f5b
Riley <rlin122006@gmail.com>
2026-08-06 17:12:57 -0400

    updated palera1n to v2.4

--- home/palera1n.nix
+++ home/palera1n.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "palera1n";
-  version = "2.3";
+  version = "2.4";
 
   src = fetchurl {
     url = "https://github.com/palera1n/palera1n/releases/download/v${version}/palera1n-linux-x86_64";
-    hash = "sha256-A3wrOYvBO6sneumruEGuPFxbyJ4iMyu8vNjQS2ghQpI=";
+    hash = "sha256-6lMd+TPAyO2rJey49D+u3QEBd0hEgrs3ROqkzxzUhlg=";
   };
 
   nativeBuildInputs = [ autoPatchelfHook ];
