8266f59ab87bc131066d6e849be10382b25431ae
Riley <rlin122006@gmail.com>
2026-08-09 18:56:20 -0400

    removed explicit screen power save script startup

--- home/hyprland/startup.lua
+++ home/hyprland/startup.lua
@@ -1,7 +1,5 @@
-local screenPowersaver = "systemctl --user restart screen-powersaver"
 local bar = "uwsm app -- qs"
 
 hl.on("hyprland.start", function ()
-    hl.exec_cmd(screenPowersaver)
     hl.exec_cmd(bar)
 end)
